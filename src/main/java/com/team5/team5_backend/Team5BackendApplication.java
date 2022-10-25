ac0ea174280f40a12fccb08cbb1d87da09ec385b
Alex1516612806 <70406534+Alex1516612806@users.noreply.github.com>
2022-10-25 14:24:18 -0500

    first commit

---
+++ src/main/java/com/team5/team5_backend/Team5BackendApplication.java
@@ -0,0 +1,41 @@
+package com.team5.team5_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+
+// To run the application use control + r
+// Minimize the run window by using command + 4
+// Stop the application use command + F2
+@SpringBootApplication
+@RestController
+public class Team5BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Team5BackendApplication.class, args);
+    }
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "names", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/teamInfo")
+    public String teamInfo() {
+        return String.format("This is team 5");
+    }
+
+    @PostMapping("/shorten")
+    public String shorten(@RequestParam(value="name",defaultValue = "World") String name){
+        return String.format("This is for shorten url" + name);
+    }
+
+    @GetMapping("/resolve")
+    public String resolve(){
+        return String.format("This is resolve");
+    }
+    @DeleteMapping("/delete")
+    void delete(){
+        
+    }
+}
