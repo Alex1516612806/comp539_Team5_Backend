279649549c47f191090345ae08384861ec141bad
chaowang <cw104@rice.edu>
2022-11-06 11:14:41 -0600

    deploy to appengine(personal account)
    1. add a new api called /demo to check if deployment can work
    2. add configuration file (app.yaml) for deployment
    3. update application.properties to application.yml

--- src/main/java/com/team5/team5_backend/Team5BackendApplication.java
+++ src/main/java/com/team5/team5_backend/Team5BackendApplication.java
@@ -1,7 +1,9 @@
 package com.team5.team5_backend;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -15,6 +17,15 @@ public class Team5BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(Team5BackendApplication.class, args);
     }
+
+    @Value("${message}")
+    String message;
+
+    @GetMapping(value = "demo")
+    public ResponseEntity<String> displayHelloMessage() {
+        return ResponseEntity.ok(message);
+    }
+
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "names", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
@@ -26,16 +37,17 @@ public class Team5BackendApplication {
     }
 
     @PostMapping("/shorten")
-    public String shorten(@RequestParam(value="name",defaultValue = "World") String name){
+    public String shorten(@RequestParam(value = "name", defaultValue = "World") String name) {
         return String.format("This is for shorten url" + name);
     }
 
     @GetMapping("/resolve")
-    public String resolve(){
+    public String resolve() {
         return String.format("This is resolve");
     }
+
     @DeleteMapping("/delete")
-    void delete(){
+    void delete() {
 
     }
 }
